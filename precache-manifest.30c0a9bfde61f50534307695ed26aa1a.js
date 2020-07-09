9a17a847779907ba4f20db37c0616b8c97be2e8b
Denys <gryshko_denys@gmail.com>
2020-07-09 19:34:47 +0300

    Updates

---
+++ precache-manifest.30c0a9bfde61f50534307695ed26aa1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9585a8c99332f47ffa9b6b68ca81769",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7064f4191d23e27a23da",
+    "url": "./static/css/main.c5e14a41.chunk.css"
+  },
+  {
+    "revision": "65678ccec73536f0c055",
+    "url": "./static/js/2.80563eaf.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.80563eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7064f4191d23e27a23da",
+    "url": "./static/js/main.e85d1df9.chunk.js"
+  },
+  {
+    "revision": "35878c0637cea8221062",
+    "url": "./static/js/runtime-main.ba67aef9.js"
+  }
+]);
